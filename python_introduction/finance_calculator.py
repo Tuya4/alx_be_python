c84bd1111754ea7a6ead836cabd997d6a8b37c6a
Mohan <mohansang04@gmail.com>
2024-06-30 19:39:04 +0300

    fixed some bugs in finance_calculator file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,17 +2,17 @@
 monthly_income = float(input("Enter your monthly income: "))
 
 # Prompt the user for their total monthly expenses
-monthly_expenses = float(input("Enter your total monthly expenses: "))
+total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate the monthly savings
-monthly_savings = int(monthly_income - monthly_expenses)
+monthly_savings = monthly_income - total_monthly_expenses
 
 # Assume an annual interest rate of 5%
 annual_interest_rate = 0.05
 
 # Calculate the projected savings after one year with interest
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate)
+projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate)
 
 # Print the results
 print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings}.")
