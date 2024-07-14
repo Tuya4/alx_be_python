f5079630a05854d385355e497a19695e59cef698
Mohan <mohansang04@gmail.com>
2024-07-14 14:07:36 +0300

    bug fix

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,8 +1,8 @@
 class BankAccount:
-    def __init__(self,balance=0):
-        self._account_balance = balance
+    def __init__(self, intial_balance=0):
+        self._account_balance = intial_balance
 
-    def deposit(self,amount):
+    def deposit(self, amount):
         if amount > 0:
             self._account_balance += amount
         else:
