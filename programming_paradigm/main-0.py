ec139636c902e354b85a15f03be2d55224568fd7
Mohan <mohansang04@gmail.com>
2024-07-14 14:03:32 +0300

    fixed bugs

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -4,12 +4,12 @@ from bank_account import BankAccount
 def main():
     account = BankAccount(100)  # Example starting balance
     if len(sys.argv) < 2:
-        print("Usage: python main.py <command>:<amount>")
+        print("Usage: python main-0.py <command>:<amount>")
         print("Commands: deposit, withdraw, display")
         sys.exit(1)
 
     command, *params = sys.argv[1].split(':')
-    amount = float(params[0]) if params else None
+    amount = int(params[0]) if params else None
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
